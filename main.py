b1749182f3fdb3803ea4d867ee52ea5ea97b305e
Daniela Salazar <dsalazara1@eafit.edu.co>
2026-05-11 13:52:51 -0500

    implementación de solveLetters (función recursiv)

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
-from itertools import zip_longest
 
 class cryptoSolver:
     matrix: np.ndarray
-    predictions: list # no pongo tipo porque tendrá el primero como int, resto como diccionarios
+    predictions: dict[list] # no pongo tipo porque tendrá el primero como int, resto como diccionarios
     letters: list[str]
+    result_in_matrix: list
+    hasSolution: bool
+    codeToDecode: list[int]
 
     def __init__(self):
         matrix =  [
@@ -16,16 +18,21 @@ class cryptoSolver:
             ['t', 'r', 'o', 'e', 'l', 's']
         ]
         self.matrix = self.normalizeMatrix(matrix)
+        print(self.matrix)
         self.letters = self.getLetters()
         print(self.letters)
+        self.result_in_matrix = self.matrix[-1]
+        self.predictions = {}
+        self.hasSolution = self.solveLetters()
     
     @staticmethod
     def normalizeMatrix(matrix: list) -> np.array:
-        # pasamos todas las listas, recorre elemento fila por fila, y cuando llega a corta, la rellena con ''
-        normalized_matrix: list = zip_longest(*matrix, fillvalue='')
 
-        # aplica transmposición porque zip_longest trabaja con ella al reves
-        return np.array(list(normalized_matrix)).T
+        max_len: int = max(len(row) for row in matrix) #obtenemos fila más larga
+        normalized_matrix: list = np.array([[''] * (max_len - len(row)) + row for row in matrix]) # rellenamos al inicio lo que les falta a las demas con '' 
+        # -> sumas se asumiran alineadas a derecha
+
+        return normalized_matrix
 
 
     def getLetters(self) -> list:
@@ -51,6 +58,7 @@ class cryptoSolver:
         return letters
 
     def getRepeatedQuantityPerColumn(self, number_repeats_per_column: list) -> None:
+        # obtiene suma de cantidad de elementos repetidos de una columna (por ejemplo, si tenemos t:3, r:2, a:1, esta funcion nos da 5, porque solo suma los que estan repetidos, a solo tiene 1, por eso no se toma en cuenta)
         for i in range(self.matrix.shape[1]):
             column: list = self.matrix[:, i]
 
@@ -64,6 +72,58 @@ class cryptoSolver:
             number_repeats_per_column.append(int(repeated_quantity))
 
         print("Total de elementos repetidos por columna:", number_repeats_per_column)
+
+    def solveLetters(self, index=0, digits_available=None):
+        if digits_available is None:
+            digits_available = list(range(10))
+        
+        # caso base
+        if index == len(self.letters):
+            return self.verifyCongruence()
+        
+        letter = self.letters[index]
+        
+        for digit in digits_available:
+            # probamos con todos los digitos disponibles en este nivel, si ninguno da, nos devolvemos.
+            if self.verifyCongruence(letter, digit):
+
+                self.predictions[letter] = digit
+                # "actualizamos" digit_avaible (en otra var) para ignorar el digit que falló
+                remaining = [d for d in digits_available if d != digit]
+
+                #intentamos con sig letra
+                if self.solveLetters(index + 1, remaining):
+                    return True
+                
+                # eliminamos predicción para intentar con otra porque en siguiente letra ninguna funcionó, entonces hay que hacer backtracking
+                self.predictions.pop(letter)
+        
+        return False
+
+    def verifyCongruence(self, letter: str, possible_value: int) -> bool:
+        
+        # verifica que valor pasado si encage en todo el result
+        pass
+    
+    @staticmethod
+    def getDigit(digits_available: list, ignore_digit: int | None) -> int | None:
+        if not digits_available:
+            return None
+
+        if ignore_digit is None:
+            return digits_available[0]
+
+        for digit in digits_available:
+            if digit != ignore_digit:
+                return digit
+
+        return None
+        
+
+    def solveCode():
+        pass
+
+    
 
 
 def main():
